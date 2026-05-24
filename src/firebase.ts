3fb357aa9d8a9963a6ab8fa724d5ac3c124927c9
Sidor01 <118011990+Sidor01@users.noreply.github.com>
2026-05-24 14:52:20 +0200

    Clear Firebase configuration values

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD1bPFzLwGY47fbfUXh04sSw5SQU5oUwXY",
-  authDomain: "kinetic-f0a5c.firebaseapp.com",
-  projectId: "kinetic-f0a5c",
-  storageBucket: "kinetic-f0a5c.firebasestorage.app",
-  messagingSenderId: "710208435474",
-  appId: "1:710208435474:web:db48c5df7424c24d352fcb"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const app = initializeApp(firebaseConfig);
